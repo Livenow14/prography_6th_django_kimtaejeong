b8c70d977288e2499b01acf9ec2d4a19700de90f
Livenow14 <xowjd41@naver.com>
2020-03-02 17:42:10 +0900

    list부분 구현

--- board/views.py
+++ board/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views import generic
-
+from .models import List
 
 #board view
 class board(generic.TemplateView):
     def get(self, request, *args, **kwargs):
         #Reserved.objects.filter(client=client_id).order_by('-check_in')
         template_name = 'board/board_list.html'
-        #기한 없는 일정, 마감 안된 애들
-
-        return render(request, template_name)
+        list=List.objects.all();
+        return render(request, template_name,{"list":list})
 
 
