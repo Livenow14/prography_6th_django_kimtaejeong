d778aa81e7d986bd85f36c31204b7ed25bb67fca
Livenow14 <xowjd41@naver.com>
2020-03-04 17:26:03 +0900

    개인 공부 파일 삭제

--- movie_api/movies/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .serializers import MovieSerializer
-from .models import Movie
-
-class MovieViewSet(viewsets.ModelViewSet):
-    queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
